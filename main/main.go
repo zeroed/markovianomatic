c06b19fa19b6e4174005304d9102bc9b92e4387a
Edoardo Rossi <edd.rossi@gmail.com>
2015-09-16 19:25:11 +0200

    Load a file from CLI

--- main/main.go
+++ main/main.go
@@ -16,12 +16,17 @@ import (
 func main() {
 	numWords := flag.Int("words", 100, "maximum number of words to print")
 	prefixLen := flag.Int("prefix", 2, "prefix length in words")
+	file := flag.String("file", "", "text file to use as seed")
 
 	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
 
 	c := markovianomatic.NewChain(*prefixLen)
-	c.Build(os.Stdin)
+	if len(*file) == 0 {
+		c.Build(os.Stdin)
+	} else {
+		c.Load(*file)
+	}
 	text := c.Generate(*numWords)
 	fmt.Println(text)
 }
