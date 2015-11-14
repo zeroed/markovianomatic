04c8dddccec5df1a45924afd7021ddec65b7b319
Edoardo Rossi <edd.rossi@gmail.com>
2015-11-14 23:18:20 +0100

    Add CLI entry point

--- main/main.go
+++ main/main.go
@@ -4,6 +4,7 @@ package main
 // go test -v -cover -tags test
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"math/rand"
@@ -17,16 +18,19 @@ func main() {
 	numWords := flag.Int("words", 100, "maximum number of words to print")
 	prefixLen := flag.Int("prefix", 2, "prefix length in words")
 	file := flag.String("file", "", "text file to use as seed")
+	verbose := flag.Bool("verbose", false, "verbose")
 
 	flag.Parse()
 	rand.Seed(time.Now().UnixNano())
 
-	c := markovianomatic.NewChain(*prefixLen)
+	c := markovianomatic.NewChain(*prefixLen, *verbose)
 	if len(*file) == 0 {
 		c.Build(os.Stdin)
 	} else {
 		c.Load(*file)
 	}
-	text := c.Generate(*numWords)
-	fmt.Println(text)
+
+	b := new(bytes.Buffer)
+	c.Generate(b, *numWords)
+	fmt.Println(b.String())
 }
