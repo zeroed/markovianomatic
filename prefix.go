6f97da58134d6012b6bb044d80c2e9ccce5e0521
Edoardo Rossi <edd.rossi@gmail.com>
2015-11-14 23:19:15 +0100

    Add builder, printer and fix Shift

--- prefix.go
+++ prefix.go
@@ -5,13 +5,20 @@ import "strings"
 // Prefix is a Markov chain prefix of one or more words.
 type Prefix []string
 
-// String returns the Prefix as a string (for use as a map key).
-func (p Prefix) String() string {
-	return strings.Join(p, " ")
+func NewPrefix(l int) Prefix {
+	if l < 2 {
+		return make(Prefix, 2)
+	}
+	return make(Prefix, l)
 }
 
 // Shift removes the first word from the Prefix and appends the given word.
 func (p Prefix) Shift(word string) {
 	copy(p, p[1:])
-	p[len(p)-1] = word
+	p[len(p)-1] = strings.TrimSpace(word)
+}
+
+// String returns the Prefix as a string (for use as a map key).
+func (p Prefix) String() string {
+	return strings.Join(p, " ")
 }
