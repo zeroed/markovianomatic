c143bc8334f86ef01ee1a4037c4993a53fd75bfe
Edoardo Rossi <edd.rossi@gmail.com>
2015-11-21 18:56:58 +0100

    Add persistence on Mongo

---
+++ model/node.go
@@ -0,0 +1,65 @@
+package model
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type Node struct {
+	Id      bson.ObjectId `bson:"_id"`
+	Key     string        `bson:"key"`
+	Choices []string      `bson:"choices"`
+}
+
+func connect() (sess *mgo.Session, err error) {
+	uri := os.Getenv("MONGODB_URL")
+	if uri == "" {
+		fmt.Fprintf(os.Stderr, "no connection string provided: using localhost\n")
+		uri = "localhost:27017"
+	}
+
+	sess, err = mgo.Dial(uri)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Can't connect to mongo, go error %v\n", err)
+		os.Exit(1)
+	}
+
+	sess.SetSafe(&mgo.Safe{})
+	return
+}
+
+func Connect(cn string) (sess *mgo.Session, coll *mgo.Collection) {
+	sess, _ = connect()
+	coll = sess.DB("markovianomatic").C(cn)
+	return
+}
+
+func NewNode(k string, v []string) *Node {
+	return &Node{
+		Id:      bson.NewObjectId(),
+		Key:     k,
+		Choices: v}
+}
+
+func (n *Node) Save(coll *mgo.Collection) bool {
+	var nn *Node
+	err := coll.Find(
+		bson.M{"key": n.Key}).One(&nn)
+
+	var operr error
+	if err == nil {
+		operr = coll.Update(
+			bson.M{"key": n.Key},
+			bson.M{"$push": bson.M{"choices": bson.M{"$each": n.Choices}}})
+	} else {
+		operr = coll.Insert(n)
+	}
+
+	if operr != nil {
+		fmt.Fprintf(os.Stderr, "Error upserting node entry: %s\n", err.Error())
+	}
+	return true
+}
